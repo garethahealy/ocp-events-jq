d087166bd0891ec7869b5902b3d34a46c6182a14
Gareth Healy <garethahealy@gmail.com>
2016-12-02 13:32:47 +0000

    Added draft of scripts

---
+++ core.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+LEVEL=$1
+if [ -z ${LEVEL+x} ]; then
+    LEVEL="Warning";
+fi
+
+echo "LEVEL == $LEVEL"
+
+PROJECTS=$(oc get projects --no-headers | cut -d' ' -f1 | xargs)
+PROJECTS_ARRAY=($PROJECTS)
+
+for project in "${PROJECTS_ARRAY[@]}" ; do
+    echo "Project: $project"
+
+    oc get events --output=json -n $project | jq '.items | .[] | select(.type=="'$LEVEL'")' |
+        jq '.metadata.name + " >> " +  .reason + " @ " + .metadata.creationTimestamp + " || " + .message'
+
+    echo
+done
---
+++ warnings.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+exec $PWD/core.sh "Warning"
