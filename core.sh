335d041b6618b477108966df2bce408ef5c8fe35
Gareth Healy <garethahealy@gmail.com>
2018-08-27 19:14:28 +0100

    Minor cleanup

--- core.sh
+++ core.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# exit when any command fails
+set -e
+
 LEVEL=$1
 if [ -z "${LEVEL+x}" ]; then
     LEVEL="Warning";
@@ -7,14 +10,16 @@ fi
 
 echo "LEVEL == $LEVEL"
 
+oc whoami --request-timeout=5s
+
 PROJECTS=$(oc get projects --no-headers | cut -d' ' -f1 | xargs)
 PROJECTS_ARRAY=($PROJECTS)
 
 for project in "${PROJECTS_ARRAY[@]}" ; do
     echo "Project: $project"
 
-    oc get events --output=json -n "$project" | jq '.items | .[] | select(.type=="'$LEVEL'")' |
+    oc get events --output=json -n "$project" |
+        jq '.items | .[] | select(.type=="'$LEVEL'")' |
         jq '.metadata.name + " >> " +  .reason + " @ " + .metadata.creationTimestamp + " || " + .message'
-
     echo
 done
